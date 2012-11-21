21e24ddb1902bce901bb25154e0ff5b4db7d20e7
Maria <mariakouppari@gmail.com>
2012-11-21 11:07:17 +0000

    events tests added

--- icsocial/events/methods.py
+++ icsocial/events/methods.py
@@ -1,8 +1,41 @@
 from django.db import models
 from events.models import *
 
-def get_event_count(binary):
-	if(binary==0):
-		return Event.objects.count()
-	elif(binary==1):
-		return Attend.objects.count()
+def get_event_count():
+	return Event.objects.count()
+
+def get_allatt_count():
+	return Attend.objects.count()
+
+def get_att_count(eve):
+	return Attend.objects.filter(event_id=eve).count()
+
+def get_user_events(atte):
+	r = []
+	bs = Attend.objects.filter(attendee=atte)
+	for b in bs:
+		r.insert(len(r),b.event_id)
+	return r
+
+
+def get_att_of_event(eve):
+	r = []
+	bs = Attend.objects.filter(event_id=eve)
+	for b in bs:
+		r.insert(len(r),b.attendee)
+	return r
+
+def get_event_titles(atte):
+   r = []
+   titles = []
+   bs = Attend.objects.filter(attendee=atte)
+   for b in bs:
+		r.insert(len(r),b.event_id)
+   for a in r:
+		obj_eve = Event.objects.filter(id=a)
+   for c in obj_eve:
+		titles.insert(len(titles),c.title)
+   return titles
+
+
+	
--- icsocial/events/tests.py
+++ icsocial/events/tests.py
@@ -5,11 +5,42 @@ from events.methods import *
 
 
 class SimpleTest(unittest.TestCase):
+
     def test_is_added(self):
-    	Event.objects.create(title='Group Meeting', location='huxley', date='2012-03-03', time='10:30', description='Work on our project.')
-    	self.assertEqual(get_event_count(0), 1)
+        Event.objects.create(title='koukou', nickname='kkokjfjfjfkos', location='huxley', date='2012-03-03', time='10:30', description='Work on our project.')
+        self.assertEqual(get_event_count(), 2)
 
-    def test_get_attend(self):
-    	Attend.objects.create(attendee=1, event_id=2)
-    	self.assertEqual(get_event_count(1), 1)
 
+    def test_is_added_att(self):
+        Attend.objects.create(attendee=2, event_id=15)
+        self.assertEqual(get_allatt_count(), 12)
+
+
+    def test_get_att_count(self):
+        Attend.objects.create(attendee=2, event_id=2)
+        self.assertEqual(get_att_count(2), 1)
+
+
+    def test_get_user_events(self):
+    	 Attend.objects.create(attendee=2, event_id=2)
+    	 Attend.objects.create(attendee=2, event_id=3)
+    	 self.assertTrue(2 in get_user_events(2))
+    	 self.assertFalse(5 in get_user_events(2))
+
+
+    def test_get_att_of_event(self):
+    	 Attend.objects.create(attendee=2, event_id=2)
+    	 Attend.objects.create(attendee=5, event_id=3)
+    	 self.assertTrue(2 in get_att_of_event(2))
+    	 self.assertTrue(5 in get_att_of_event(3))
+    	 self.assertFalse(2 in get_att_of_event(3))
+
+    def test_get_event_titles(self):
+        Event.objects.create(title='koukou', nickname='kkokkos', location='huxley', date='2012-03-03', time='10:30', description='Work on our project.')
+        Attend.objects.create(attendee=1, event_id=1)
+        Attend.objects.create(attendee=1, event_id=2)
+        Attend.objects.create(attendee=1, event_id=3)
+        Attend.objects.create(attendee=1, event_id=4)
+        Attend.objects.create(attendee=1, event_id=5)
+        Attend.objects.create(attendee=1, event_id=6)
+        self.assertFalse('koukou' in get_event_titles(1))
